bc4eacd75b39bec1b7c1e74c23d231deeb6f8705
Robert DeJaco <dejac001@umn.edu>
2020-04-15 15:09:54 -0400

    Add requirements.txt for docs and update conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,7 +31,16 @@ release = 'v0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark',
+    'sphinx.ext.autodoc', 'sphinx.ext.coverage',
+    'sphinx.ext.todo', 'sphinx.ext.viewcode',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.githubpages', #'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
+    'sphinxcontrib.bibtex'
 ]
+
+todo_include_todos = True
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +56,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
